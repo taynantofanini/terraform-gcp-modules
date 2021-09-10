129a403e9fd1a3399732da2ded6a6af42d7f936c
Taynan Tofanini <taynan@santodigital.com.br>
2021-09-09 22:08:56 -0300

    fix dns provider

--- dns/dns.tf
+++ dns/dns.tf
@@ -1,5 +1,4 @@
 resource "google_dns_managed_zone" "dns-zone" {
-  provider = google.google
   name     = var.zone_name
   dns_name = var.dns_name
   labels = {
@@ -9,7 +8,6 @@ resource "google_dns_managed_zone" "dns-zone" {
 
 resource "google_dns_record_set" "dns-record" {
   for_each     = var.record_set
-  provider     = google-beta.google-beta
   managed_zone = google_dns_managed_zone.google_dns_zone.name
   name         = each.value.name
   type         = each.value.type
--- dns/provider.tf
+++ dns/provider.tf
@@ -1,8 +1,3 @@
 provider "google-beta" {
   project = var.project
-  alias   = "google-beta"
-}
-
-provider "google" {
-  project = var.project
 }
